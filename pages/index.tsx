8f398c6760621e51936c33df5b164285e27c15e8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-03 16:22:34 +0900

    modify index

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,19 @@ import { Auth } from '../components/Auth'
 import { DashBoard } from '../components/DashBoard'
 
 const Home: NextPage = () => {
-  return <div></div>
+  const session = useStore((state) => state.session)
+  const setSession = useStore((state) => state.setSesstion)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [setSession])
+
+  return (
+    <Layout title="Dashboard">{!session ? <Auth /> : <DashBoard />}</Layout>
+  )
 }
 
 export default Home
