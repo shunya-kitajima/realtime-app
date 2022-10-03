a9089cef7b16270863b71a72fc0d5022471f6a07
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-03 16:00:12 +0900

    import modules

---
+++ components/Layout.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from 'react'
+import Head from 'next/head'
+import { CheckBadgeIcon } from '@heroicons/react/24/solid'
+
+export const Layout: React.FC = () => {
+  return <div>Layout</div>
+}
