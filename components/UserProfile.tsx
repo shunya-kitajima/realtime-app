73e57aacf9a9fa142a713736f43a8a6728d62656
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-04 15:37:44 +0900

    create input form

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -10,5 +10,66 @@ import { useUploadAvatarImg } from '../hooks/useUploadAvatarImg'
 import { Spinner } from './Spinner'
 
 export const UserProfile: React.FC = () => {
-  return <div>UserProfile</div>
+  const session = useStore((state) => state.session)
+  const editedProfile = useStore((state) => state.editedprofile)
+  const update = useStore((state) => state.updateEditedProfile)
+  const { data: profile } = useQueryProfile()
+  const { updateProfileMutation } = useMutateProfile()
+  const { useMutateUploadAvatarImg } = useUploadAvatarImg()
+  const { fullUrl: avatarUrl, isLoading } = useDownloadUrl(
+    editedProfile.avatar_url,
+    'avatars'
+  )
+
+  const updateProfile = () => {
+    updateProfileMutation.mutate({
+      id: session?.user?.id,
+      username: editedProfile.username,
+      avatar_url: editedProfile.avatar_url,
+      favorites: editedProfile.favorites,
+    })
+  }
+
+  return (
+    <>
+      <p className="mb-4">{profile?.username}</p>
+      {profile?.created_at && (
+        <p className="my-1 text-sm">
+          {format(new Date(profile.created_at), 'yyyy-MM-dd HH:mm:ss')}
+        </p>
+      )}
+      {profile?.updated_at && (
+        <p className="my-1 text-sm">
+          {format(new Date(profile.updated_at), 'yyyy-MM-dd HH:mm:ss')}
+        </p>
+      )}
+      <p className="mt-4">Username</p>
+      <input
+        className="my-2 mx-2 rounded border border-gray-300 px-2 py-2 text-sm focus:outline-none"
+        type="text"
+        value={editedProfile.username || ''}
+        onChange={(e) => update({ ...editedProfile, username: e.target.value })}
+      />
+      <p className="mt-4">Favorites</p>
+      <input
+        className="my-2 mx-2 rounded border border-gray-300 px-2 py-2 text-sm focus:outline-none"
+        type="text"
+        value={editedProfile.favorites || ''}
+        onChange={(e) =>
+          update({ ...editedProfile, favorites: e.target.value })
+        }
+      />
+      <button
+        className={`my-5 rounded ${
+          updateProfileMutation.isLoading || !editedProfile.username
+            ? 'bg-gray-400'
+            : 'bg-indigo-600'
+        } px-3 py-2 text-sm font-medium text-white`}
+        onClick={updateProfile}
+        disabled={updateProfileMutation.isLoading || !editedProfile.username}
+      >
+        {updateProfileMutation.isLoading ? '...Loading' : 'Update'}
+      </button>
+    </>
+  )
 }
