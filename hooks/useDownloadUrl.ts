d45d9dac19b7f1fb28893f82e3e863539d1e8185
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-04 15:03:58 +0900

    import modules

---
+++ hooks/useDownloadUrl.ts
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react'
+import { supabase } from '../utiles/supabase'
+
+export const useDownloadUrl = (
+  filePath: string | undefined,
+  key: 'avatars' | 'posts'
+) => {
+  return {}
+}
