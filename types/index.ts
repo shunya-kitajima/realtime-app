cddd683a20bd5431e2231f65ea6a4dd8c2e69c6d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-04 13:39:48 +0900

    add file and create Profile type

---
+++ types/index.ts
@@ -0,0 +1,8 @@
+export type Profile = {
+  id: string | undefined
+  created_at: string
+  updated_at: string
+  username: string | undefined
+  avatar_url: string | undefined
+  favorites: string | undefined
+}
