91728c62f80b5f274311f8fef9599fd99cff8440
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-04 15:10:13 +0900

    set useEffect

--- hooks/useDownloadUrl.ts
+++ hooks/useDownloadUrl.ts
@@ -5,5 +5,28 @@ export const useDownloadUrl = (
   filePath: string | undefined,
   key: 'avatars' | 'posts'
 ) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [fullUrl, setFullUrl] = useState('')
+  const bucketName = key === 'avatars' ? 'avatars' : 'posts'
+
+  useEffect(() => {
+    if (filePath) {
+      const download = async () => {
+        setIsLoading(true)
+        const { data, error } = await supabase.storage
+          .from(bucketName)
+          .download(filePath)
+        if (error) {
+          setIsLoading(false)
+          throw new Error(error.message)
+        }
+        setFullUrl(URL.createObjectURL(data!))
+        setIsLoading(false)
+
+        download()
+      }
+    }
+  }, [filePath, bucketName])
+
   return {}
 }
