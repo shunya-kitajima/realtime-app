3a208064f27955994b299ec586e92899f0e23de0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-04 13:48:23 +0900

    add state of profile for store

--- store/index.ts
+++ store/index.ts
@@ -1,14 +1,29 @@
 import create from 'zustand'
 import { Session } from '@supabase/supabase-js'
+import { EditedProfile } from '../types'
 
 type State = {
   session: Session | null
   setSesstion: (payload: Session | null) => void
+  editedprofile: EditedProfile
+  updateEditedProfile: (payload: EditedProfile) => void
+  resetEditedProfile: () => void
 }
 
 const useStore = create<State>((set) => ({
   session: null,
   setSesstion: (payload) => set({ session: payload }),
+  editedprofile: { username: '', avatar_url: '', favorites: '' },
+  updateEditedProfile: (payload) =>
+    set({
+      editedprofile: {
+        username: payload.username,
+        avatar_url: payload.avatar_url,
+        favorites: payload.favorites,
+      },
+    }),
+  resetEditedProfile: () =>
+    set({ editedprofile: { username: '', avatar_url: '', favorites: '' } }),
 }))
 
 export default useStore
