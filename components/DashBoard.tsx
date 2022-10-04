298b725a35b189a9d75340ff66010118333c70ed
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-04 15:50:55 +0900

    import modules and add userprofile component

--- components/DashBoard.tsx
+++ components/DashBoard.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 import {
   ArrowRightOnRectangleIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/solid'
 import { supabase } from '../utiles/supabase'
+import { Spinner } from './Spinner'
+import { UserProfile } from './UserProfile'
 
 export const DashBoard: React.FC = () => {
   const signOut = () => {
@@ -16,6 +19,17 @@ export const DashBoard: React.FC = () => {
         className="my-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
       />
+      <div className="flex flex-col items-center">
+        <ErrorBoundary
+          fallback={
+            <ExclamationCircleIcon className="my-5 h-10 text-pink-500" />
+          }
+        >
+          <Suspense fallback={<Spinner />}>
+            <UserProfile />
+          </Suspense>
+        </ErrorBoundary>
+      </div>
     </div>
   )
 }
