736335a2530ea45cbf7984c055cdf9f472d64f3e
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-04 14:43:51 +0900

    create getProfile and useQuery

--- hooks/useQueryProfile.ts
+++ hooks/useQueryProfile.ts
@@ -10,7 +10,40 @@ export const useQueryProfile = () => {
   const update = useStore((state) => state.updateEditedProfile)
   const { createProfileMutation } = useMutateProfile()
 
-  const getProfile = async () => {}
+  const getProfile = async () => {
+    const { data, error, status } = await supabase
+      .from('profiles')
+      .select('*')
+      .eq('id', session?.user?.id)
+      .single()
 
-  return {}
+    if (error && status === 406) {
+      createProfileMutation.mutate({
+        id: session?.user?.id,
+        username: session?.user?.email,
+        avatar_url: '',
+        favorites: '',
+      })
+      update({
+        ...editedProfile,
+        username: session?.user?.email,
+      })
+    }
+    if (error && status !== 406) throw new Error(error.message)
+    return data
+  }
+
+  return useQuery<Profile, Error>({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+    staleTime: Infinity,
+    onSuccess: (data) => {
+      if (data)
+        update({
+          username: data.username,
+          avatar_url: data.avatar_url,
+          favorites: data.favorites,
+        })
+    },
+  })
 }
