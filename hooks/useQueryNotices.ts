925e51a718408ffba43c623302e4d2e0740dd033
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 14:36:22 +0900

    create query of notice

--- hooks/useQueryNotices.ts
+++ hooks/useQueryNotices.ts
@@ -1,4 +1,20 @@
 import { useQuery } from 'react-query'
 import { supabase } from '../utiles/supabase'
-import useStore from '../store'
 import { Notice } from '../types'
+
+export const useQueryNotices = () => {
+  const getNotices = async () => {
+    const { data, error } = await supabase
+      .from('notices')
+      .select('*')
+      .order('created_at', { ascending: true })
+    if (error) throw new Error(error.message)
+    return data
+  }
+
+  return useQuery<Notice[], Error>({
+    queryKey: ['notices'],
+    queryFn: getNotices,
+    staleTime: Infinity,
+  })
+}
