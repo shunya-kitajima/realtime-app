2868d2fadf2789149329683c13c4f5b67845132e
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 17:33:29 +0900

    import modules

---
+++ components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useQueryNotices } from '../hooks/useQueryNotices'
+import { useSubscribeNotices } from '../hooks/useSubscribeNotices'
+import { NoticeFormMemo } from './NoticeForm'
+import { NoticeItemMemo } from './NoticeItem'
+
+export const Notification: React.FC = () => {
+  return <div>Notification</div>
+}
