9d774c7510c35355e64b9a4c3086cdb3c663b2d0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 14:56:29 +0900

    import modules

--- hooks/useSubscribeNotices.ts
+++ hooks/useSubscribeNotices.ts
@@ -1,4 +1,8 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useQueryClient } from 'react-query'
+import { SupabaseRealtimePayload } from '@supabase/supabase-js'
+import { supabase } from '../utiles/supabase'
+import { Notice } from '../types'
 
 export const useSubscribeNotices = () => {
   return {}
