0feb05cfeb32e547bb090ad2cb968e5b803ebef9
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 17:46:31 +0900

    import Notification component

--- components/DashBoard.tsx
+++ components/DashBoard.tsx
@@ -9,14 +9,18 @@ import { supabase } from '../utiles/supabase'
 import useStore from '../store'
 import { Spinner } from './Spinner'
 import { UserProfile } from './UserProfile'
+import { Notification } from './Notification'
 
 export const DashBoard: React.FC = () => {
   const queryClient = useQueryClient()
-  const reset = useStore((state) => state.resetEditedProfile)
+  const resetEditedProfile = useStore((state) => state.resetEditedProfile)
+  const resetEditedNotice = useStore((state) => state.resetEditedNotice)
 
   const signOut = () => {
-    reset()
+    resetEditedProfile()
+    resetEditedNotice()
     queryClient.removeQueries(['profile'])
+    queryClient.removeQueries(['notices'])
     supabase.auth.signOut()
   }
 
@@ -26,16 +30,29 @@ export const DashBoard: React.FC = () => {
         className="my-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
       />
-      <div className="flex flex-col items-center">
-        <ErrorBoundary
-          fallback={
-            <ExclamationCircleIcon className="my-5 h-10 text-pink-500" />
-          }
-        >
-          <Suspense fallback={<Spinner />}>
-            <UserProfile />
-          </Suspense>
-        </ErrorBoundary>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col items-center">
+          <ErrorBoundary
+            fallback={
+              <ExclamationCircleIcon className="my-5 h-10 text-pink-500" />
+            }
+          >
+            <Suspense fallback={<Spinner />}>
+              <UserProfile />
+            </Suspense>
+          </ErrorBoundary>
+        </div>
+        <div className="flex flex-col items-center">
+          <ErrorBoundary
+            fallback={
+              <ExclamationCircleIcon className="my-5 h-10 text-pink-500" />
+            }
+          >
+            <Suspense fallback={<Spinner />}>
+              <Notification />
+            </Suspense>
+          </ErrorBoundary>
+        </div>
       </div>
     </>
   )
