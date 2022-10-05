df3b09d9a116cdba9511414fedbd8f460984ce65
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 21:16:10 +0900

    create query of comment

--- hooks/useQueryComments.ts
+++ hooks/useQueryComments.ts
@@ -1,3 +1,20 @@
 import { useQuery } from 'react-query'
 import { supabase } from '../utiles/supabase'
 import { Comment } from '../types'
+
+export const useQueryComments = () => {
+  const getComments = async () => {
+    const { data, error } = await supabase
+      .from('comments')
+      .select('*')
+      .order('id', { ascending: true })
+    if (error) throw new Error(error.message)
+    return data
+  }
+
+  return useQuery<Comment[], Error>({
+    queryKey: ['comments'],
+    queryFn: getComments,
+    staleTime: Infinity,
+  })
+}
