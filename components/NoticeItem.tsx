19bdc6e1a4c08fd4345b3e9699e67e831cad64fb
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 15:49:35 +0900

    import modules

---
+++ components/NoticeItem.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode, memo } from 'react'
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
+import useStore from '../store'
+import { useMutateNotice } from '../hooks/useMutateNotice'
+import { Notice } from '../types'
+
+export const NoticeItem: React.FC = () => {
+  return <div>NoticeItem</div>
+}
