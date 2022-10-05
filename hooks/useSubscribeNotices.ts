f6d8eb0b77080bd8542ac4576bac2e6be1eb2fcc
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 15:18:02 +0900

    create update cache update

--- hooks/useSubscribeNotices.ts
+++ hooks/useSubscribeNotices.ts
@@ -26,8 +26,31 @@ export const useSubscribeNotices = () => {
           ]
         )
       })
-      .on('UPDATE', (payload: SupabaseRealtimePayload<Notice>) => {})
-      .on('DELETE', (payload: SupabaseRealtimePayload<Notice>) => {})
+      .on('UPDATE', (payload: SupabaseRealtimePayload<Notice>) => {
+        let previousNotices = queryClient.getQueryData<Notice[]>(['notices'])
+        if (!previousNotices) previousNotices = []
+        queryClient.setQueryData(
+          ['notices'],
+          previousNotices.map((notice) =>
+            notice.id === payload.new.id
+              ? {
+                  id: payload.new.id,
+                  created_at: payload.new.created_at,
+                  content: payload.new.content,
+                  user_id: payload.new.user_id,
+                }
+              : notice
+          )
+        )
+      })
+      .on('DELETE', (payload: SupabaseRealtimePayload<Notice>) => {
+        let previousNotices = queryClient.getQueryData<Notice[]>(['notices'])
+        if (!previousNotices) previousNotices = []
+        queryClient.setQueryData(
+          ['notices'],
+          previousNotices.filter((notice) => notice.id !== payload.old.id)
+        )
+      })
       .subscribe()
     const removeSubscription = async () => {
       await supabase.removeSubscription(subsc)
