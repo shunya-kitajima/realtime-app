b45c65cd56decea14f92c417d0920d7a156df2cf
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 14:53:00 +0900

    create mutations of notice

---
+++ hooks/useMutateNotice.ts
@@ -0,0 +1,68 @@
+import { useQueryClient, useMutation } from 'react-query'
+import { supabase } from '../utiles/supabase'
+import useStore from '../store'
+import { Notice, EditedNotice } from '../types'
+
+export const useMutateNotice = () => {
+  const queryClient = useQueryClient()
+  const reset = useStore((state) => state.resetEditedNotice)
+
+  const createNoticeMutation = useMutation(
+    async (notice: Omit<Notice, 'id' | 'create_at'>) => {
+      const { data, error } = await supabase.from('notices').insert(notice)
+      if (error) throw new Error(error.message)
+      return data
+    },
+    {
+      onSuccess: () => {
+        reset()
+      },
+      onError: (err: any) => {
+        alert(err.message)
+        reset()
+      },
+    }
+  )
+
+  const updateNoticeMutation = useMutation(
+    async (notice: EditedNotice) => {
+      const { data, error } = await supabase
+        .from('notices')
+        .update({ content: notice.content })
+        .eq('id', notice.id)
+      if (error) throw new Error(error.message)
+      return data
+    },
+    {
+      onSuccess: () => {
+        reset()
+      },
+      onError: (err: any) => {
+        alert(err.message)
+        reset()
+      },
+    }
+  )
+
+  const deleteNoticeMutation = useMutation(
+    async (id: string) => {
+      const { data, error } = await supabase
+        .from('notices')
+        .delete()
+        .eq('id', id)
+      if (error) throw new Error(error.message)
+      return data
+    },
+    {
+      onSuccess: () => {
+        reset()
+      },
+      onError: (err: any) => {
+        alert(err.message)
+        reset()
+      },
+    }
+  )
+
+  return {}
+}
