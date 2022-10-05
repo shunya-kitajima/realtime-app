77a314e85ee10d89f8c22e664a51bca6460dcab4
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 15:12:12 +0900

    create update cache insert

--- hooks/useSubscribeNotices.ts
+++ hooks/useSubscribeNotices.ts
@@ -5,5 +5,37 @@ import { supabase } from '../utiles/supabase'
 import { Notice } from '../types'
 
 export const useSubscribeNotices = () => {
+  const queryClient = useQueryClient()
+
+  useEffect(() => {
+    const subsc = supabase
+      .from('notices')
+      .on('INSERT', (payload: SupabaseRealtimePayload<Notice>) => {
+        let previousNotices = queryClient.getQueryData<Notice[]>(['notices'])
+        if (!previousNotices) previousNotices = []
+        queryClient.setQueryData<Notice[]>(
+          ['notices'],
+          [
+            ...previousNotices,
+            {
+              id: payload.new.id,
+              created_at: payload.new.created_at,
+              content: payload.new.content,
+              user_id: payload.new.user_id,
+            },
+          ]
+        )
+      })
+      .on('UPDATE', (payload: SupabaseRealtimePayload<Notice>) => {})
+      .on('DELETE', (payload: SupabaseRealtimePayload<Notice>) => {})
+      .subscribe()
+    const removeSubscription = async () => {
+      await supabase.removeSubscription(subsc)
+    }
+    return () => {
+      removeSubscription()
+    }
+  }, [queryClient])
+
   return {}
 }
