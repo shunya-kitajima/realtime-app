e04b549117ffead4c15904d4bb09d5cceab253a2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 17:30:50 +0900

    create and memo NoticeItem component

--- components/NoticeItem.tsx
+++ components/NoticeItem.tsx
@@ -1,9 +1,39 @@
-import React, { ReactNode, memo } from 'react'
+import React, { memo } from 'react'
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
 import useStore from '../store'
 import { useMutateNotice } from '../hooks/useMutateNotice'
 import { Notice } from '../types'
 
-export const NoticeItem: React.FC = () => {
-  return <div>NoticeItem</div>
+const NoticeItem: React.FC<Omit<Notice, 'created_at'>> = ({
+  id,
+  content,
+  user_id,
+}) => {
+  const session = useStore((state) => state.session)
+  const update = useStore((state) => state.updateEditedNotice)
+  const { deleteNoticeMutation } = useMutateNotice()
+
+  return (
+    <li className="my-3">
+      <span>{content}</span>
+      {session?.user?.id === user_id && (
+        <div className="float-right ml-20 flex">
+          <PencilIcon
+            className="mx-1 h-5 w-5 cursor-pointer text-blue-500"
+            onClick={() => {
+              update({ id: id, content: content })
+            }}
+          />
+          <TrashIcon
+            className="h-5 w-5 cursor-pointer text-blue-500"
+            onClick={() => {
+              deleteNoticeMutation.mutate(id)
+            }}
+          />
+        </div>
+      )}
+    </li>
+  )
 }
+
+export const NoticeItemMemo = memo(NoticeItem)
