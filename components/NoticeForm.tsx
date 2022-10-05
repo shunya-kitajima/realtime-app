132ffd9fd46168342a0409b38e711c0cb902ff9d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-05 15:40:15 +0900

    create form

--- components/NoticeForm.tsx
+++ components/NoticeForm.tsx
@@ -8,5 +8,38 @@ export const NoticeForm: React.FC = () => {
   const update = useStore((state) => state.updateEditedNotice)
   const { createNoticeMutation, updateNoticeMutation } = useMutateNotice()
 
-  return <div>NoticeForm</div>
+  const SubmitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedNotice.id === '') {
+      createNoticeMutation.mutate({
+        content: editedNotice.content,
+        user_id: session?.user?.id,
+      })
+    } else {
+      updateNoticeMutation.mutate(editedNotice)
+    }
+  }
+
+  return (
+    <form onSubmit={SubmitHandler}>
+      <input
+        type="text"
+        className="my-1 rounded border border-gray-300 px-3 py-2 text-sm focus:outline-none"
+        placeholder="New Notice ?"
+        value={editedNotice.content}
+        onChange={(e) => update({ ...editedNotice, content: e.target.value })}
+      />
+      <div className="my-3 flex justify-center">
+        <button
+          type="submit"
+          data-testid="btn-notice"
+          className={`rounded ${
+            editedNotice.content ? 'bg-indigo-600' : 'bg-gray-300'
+          } px-3 py-2 text-sm text-white`}
+        >
+          {editedNotice.id ? 'Update' : 'Create'}
+        </button>
+      </div>
+    </form>
+  )
 }
