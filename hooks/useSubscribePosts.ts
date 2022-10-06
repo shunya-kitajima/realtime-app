fe88121a19688ee33c495d6700a3d1fad555e04e
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-06 10:28:17 +0900

    create useEffect of insert

--- hooks/useSubscribePosts.ts
+++ hooks/useSubscribePosts.ts
@@ -5,5 +5,29 @@ import { supabase } from '../utiles/supabase'
 import { Post } from '../types'
 
 export const useSubscribePosts = () => {
-  return {}
+  const queryClient = useQueryClient()
+
+  useEffect(() => {
+    const subsc = supabase
+      .from('posts')
+      .on('INSERT', (payload: SupabaseRealtimePayload<Post>) => {
+        let previousPosts = queryClient.getQueryData<Post[]>(['posts'])
+        if (!previousPosts) previousPosts = []
+        queryClient.setQueryData(
+          ['posts'],
+          [
+            ...previousPosts,
+            {
+              id: payload.new.id,
+              created_at: payload.new.created_at,
+              title: payload.new.title,
+              post_url: payload.new.post_url,
+              user_id: payload.new.user_id,
+            },
+          ]
+        )
+      })
+      .on('UPDATE', (payload: SupabaseRealtimePayload<Post>) => {})
+      .on('DELETE', (payload: SupabaseRealtimePayload<Post>) => {})
+  }, [])
 }
