52463137159dbb62e183baca91a68adc1df88d29
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-06 17:19:42 +0900

    add file and import modules

---
+++ components/Feed.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useQueryPosts } from '../hooks/useQueryPosts'
+import { useSubscribePosts } from '../hooks/useSubscribePosts'
+import { PostFormMemo } from './PostForm'
+import { PostItemMemo } from './PostItem'
+
+export const Feed: React.FC = () => {
+  return <div>Feed</div>
+}
