6473d28417c9d849d28754755d3dfaa0d90168de
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-06 10:20:55 +0900

    import modules

---
+++ hooks/useSubscribePosts.ts
@@ -0,0 +1,9 @@
+import { useEffect } from 'react'
+import { useQueryClient } from 'react-query'
+import { SupabaseRealtimePayload } from '@supabase/supabase-js'
+import { supabase } from '../utiles/supabase'
+import { Post } from '../types'
+
+export const useSubscribePosts = () => {
+  return {}
+}
