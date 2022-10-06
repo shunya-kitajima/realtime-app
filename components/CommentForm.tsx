7cdcaff1b883c4c9e0e46a102117b9fd98eedb56
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-06 18:35:00 +0900

    import modules

--- components/CommentForm.tsx
+++ components/CommentForm.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { FormEvent, Dispatch, SetStateAction, memo } from 'react'
+import { EnvelopeIcon } from '@heroicons/react/24/solid'
+import useStore from '../store'
+import { useMutateComment } from '../hooks/useMutateComment'
+import { EditedComment } from '../types'
 
-export const CommentForm = () => {
+const CommentForm: React.FC = () => {
   return <div>CommentForm</div>
 }
+
+export const CommentFormMemo = memo(CommentForm)
