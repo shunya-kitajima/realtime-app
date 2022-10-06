22958cf5952a08b85a27312ed61ba0a69f972e02
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-06 17:26:32 +0900

    create Feed component

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -5,5 +5,24 @@ import { PostFormMemo } from './PostForm'
 import { PostItemMemo } from './PostItem'
 
 export const Feed: React.FC = () => {
-  return <div>Feed</div>
+  const { data: posts } = useQueryPosts()
+  useSubscribePosts()
+
+  return (
+    <>
+      <p className="mb-4 text-center">Feed</p>
+      <PostFormMemo />
+      <ul data-testid="ul-post" className="my-5">
+        {posts?.map((post) => (
+          <PostItemMemo
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            post_url={post.post_url}
+            user_id={post.user_id}
+          />
+        ))}
+      </ul>
+    </>
+  )
 }
