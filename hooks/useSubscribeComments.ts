3bf6b016889b998ba40675ff775c3ff24b0361bd
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-06 10:37:30 +0900

    import modules

---
+++ hooks/useSubscribeComments.ts
@@ -0,0 +1,5 @@
+import { useEffect } from 'react'
+import { useQueryClient } from 'react-query'
+import { SupabaseRealtimePayload } from '@supabase/supabase-js'
+import { supabase } from '../utiles/supabase'
+import { Comment } from '../types'
