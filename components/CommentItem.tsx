a5da68f5835ee8d3def43484128b22c801f7f69a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-06 19:08:14 +0900

    create avatar

--- components/CommentItem.tsx
+++ components/CommentItem.tsx
@@ -26,8 +26,26 @@ const CommentItem: React.FC<Props> = ({
 }) => {
   const session = useStore((state) => state.session)
   const { deleteCommentMutation } = useMutateComment()
+  const { data } = useQueryAvatar(user_id)
+  const { fullUrl: avatarUrl } = useDownloadUrl(data?.avatar_url, 'avatars')
 
-  return <div>CommentItem</div>
+  return (
+    <li className="my-3 flex items-center justify-between">
+      <div className="flex">
+        {avatarUrl ? (
+          <Image
+            src={avatarUrl}
+            alt="avatar"
+            className="rounded-full"
+            width={25}
+            height={25}
+          />
+        ) : (
+          <UserCircleIcon className="inline-block h-8 w-8 cursor-pointer text-gray-500" />
+        )}
+      </div>
+    </li>
+  )
 }
 
 export const CommentItemMemo = memo(CommentItem)
