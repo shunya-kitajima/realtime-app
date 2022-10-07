b42660b487812a9471400d96e07d7f665048d168
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-07 16:47:21 +0900

    create test template

---
+++ cypress/integration/auth.spec.ts
@@ -0,0 +1,8 @@
+/// <reference types="cypress"/>
+describe('Authentication', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('Shall navigate to DashBoard when successully login', () => {})
+})
+export {}
