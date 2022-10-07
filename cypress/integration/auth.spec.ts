ddef8b2e7e2da3b0984df3b58f47a996e93d51d2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-07 16:53:37 +0900

    create login test

--- cypress/integration/auth.spec.ts
+++ cypress/integration/auth.spec.ts
@@ -3,6 +3,11 @@ describe('Authentication', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it('Shall navigate to DashBoard when successully login', () => {})
+  it('Shall navigate to DashBoard when successully login', () => {
+    cy.get('input[placeholder="Email"]').type('user1@test.com')
+    cy.get('input[placeholder="Password"]').type('password')
+    cy.get('[type="submit"]').click()
+    cy.get('[data-testid="logout"]').should('be.visible')
+  })
 })
 export {}
