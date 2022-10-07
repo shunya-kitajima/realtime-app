fbe804a28f3df878e65b578b127b04a430e163fd
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-07 15:27:32 +0900

    import modules

---
+++ components/Comments.tsx
@@ -0,0 +1,12 @@
+import React, { useState, memo } from 'react'
+import { useQueryComments } from '../hooks/useQueryComments'
+import { useSubscribeComments } from '../hooks/useSubscribeComments'
+import { CommentFormMemo } from './CommentForm'
+import { CommentItemMemo } from './CommentItem'
+import { EditedComment } from '../types'
+
+const Comments: React.FC = () => {
+  return <div>Comments</div>
+}
+
+export const CommentsMemo = memo(Comments)
